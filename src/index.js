70bced6bfd6e8486018da913205667d6e6f3141c
Jake Hunter <jake7hunter@hmail.com>
2022-02-16 02:28:05 -0500

    sent form info to database

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 // Variables
 const jokeButton = document.getElementById(`jokeButton`);
 const currentJoke = document.getElementById(`currentJoke`);
+const form = document.getElementById(`form`)
+const yourName = document.getElementById(`yourName`)
+const yourJoke = document.getElementById(`yourJoke`)
+const jokeEl = document.getElementById(`submittedJoke`)
+const db = firebase.firestore()
 
 // Joke Maker
 jokeButton.addEventListener(`click`, function(){
     getNewJoke();
-    setNewColour();
 })
 
 function getNewJoke(){
@@ -29,3 +33,46 @@ function getNewJoke(){
 function createNewJoke(response){
     currentJoke.innerHTML = `<p>${response.joke}</p>`;
 }
+
+// Form
+form.addEventListener(`submit`, function(event){
+    event.preventDefault();
+
+    if(yourName.value && yourJoke.value){
+        addJoke(yourName.value, yourJoke.value)
+    }
+
+    function addJoke(Name, Joke){
+        db.collection(`Jokes`)
+        .add({
+            your_name: Name,
+            your_joke: Joke,
+        })
+        .then(function(docref){
+            console.log(`document written with id:`, docref.id);
+        })
+        .catch(function (err) {
+            console.log(`error adding doc: `, err);
+        })
+    }
+
+    function getJokes() {
+        db.collection(`Jokes`)
+        .get()
+        .then((querySnapshot)=>{
+            let output = "";
+
+            querySnapshot.forEach((doc) => {
+                console.log(doc.data());
+                output += `<li>${doc.data().your_name}: ${doc.data().your_joke}</li>`; 
+            });
+
+            jokeEl.innerHTML = output;
+        })
+        .catch(function (err) {
+            console.log(`error adding doc: `, err);
+        })
+    }
+
+    getJokes();
+});
